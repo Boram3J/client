1e94800fac3811f33c81a904e3639bbd020c2748
InYoung Park <dlsdudg15@naver.com>
2022-08-22 17:38:51 +0900

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FaGithub, FaPhone } from 'react-icons/fa';
+import { AiFillMail } from 'react-icons/ai';
 
 import * as Styled from './styled';
 
@@ -6,10 +8,26 @@ export default function Footer() {
   return (
     <Styled.Wrapper>
       <Styled.Container>
-        Made by{' '}
-        <a style={{ color: '#1fb6ff' }} href='https://github.com/Boram3J'>
-          Boram3J
-        </a>
+        <div className='flex items-center'>
+          <div className='mr-1'>
+            <FaGithub />
+          </div>
+          <a style={{ color: '#1fb6ff' }} href='https://github.com/Boram3J'>
+            Boram3J
+          </a>
+        </div>
+        <div className='flex items-center'>
+          <div className='mr-1'>
+            <FaPhone />
+          </div>
+          <div>010-2717-2868</div>
+        </div>
+        <div className='flex items-center'>
+          <div className='mr-1'>
+            <AiFillMail />
+          </div>
+          <div>dlsdudg15@naver.com</div>
+        </div>
       </Styled.Container>
     </Styled.Wrapper>
   );
